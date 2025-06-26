6bf240c8fb61c551ba14cc4ed2f747edc3dbe4ba
EliteArtists <164914828+EliteArtists@users.noreply.github.com>
2025-06-26 12:00:49 +0100

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://dxukzjeoeoapqsnuczxs.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dWt6amVvZW9hcHFzbnVjenhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MTczNjIsImV4cCI6MjA2NTQ5MzM2Mn0.45hPgE4InKVrW5LZIgmKoTyJ9w8nkb5Z4tOeJC_0vhw'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
